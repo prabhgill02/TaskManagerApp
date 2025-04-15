f11271d8a340950bab96f09eac114e5832c2ee4e
prabhgill02 <prabhdeepsinghgill1@gmail.com>
2025-04-15 19:01:08 -0400

    infra deployed

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,11 +1,35 @@
-
 resource "aws_s3_bucket" "frontend" {
   bucket = var.bucket_name
-  acl    = "public-read"
 
   website {
     index_document = "index.html"
   }
 
   tags = var.tags
+}
+
+resource "aws_s3_bucket_public_access_block" "public" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend.arn}/*"
+      }
+    ]
+  })
 }
