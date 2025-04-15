971c9861bdc3e53e259f82a1dbff38615dd534da
prabhgill02 <prabhdeepsinghgill1@gmail.com>
2025-04-15 18:31:08 -0400

    updated files

--- terraform/modules/ec2/user_data.sh
+++ terraform/modules/ec2/user_data.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt install -y nodejs npm git
-git clone https://github.com/YOUR_USER/YOUR_REPO.git /home/ubuntu/app
-cd /home/ubuntu/app/backend
+git clone https://github.com/prabhgill02/TaskManagerApp.git /home/ubuntu/app
+cd /home/ubuntu/app/TaskManagerApp/backend
 npm install
 nohup node server.js > app.log 2>&1 &
