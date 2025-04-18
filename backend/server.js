53d81c2fe64a656371c49fd42066552a1b2b5b4f
prabhgill02 <prabhdeepsinghgill1@gmail.com>
2025-04-18 18:48:51 -0400

    Updated backend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,15 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+// Updated the code so that is accessible on all other interfaces as well
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on http://0.0.0.0:3000');
 });
+
