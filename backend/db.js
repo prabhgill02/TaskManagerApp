f1854447ce4f836204bdd5da70c8befb46ab8393
prabhgill02 <prabhdeepsinghgill1@gmail.com>
2025-04-18 18:38:39 -0400

    Updated backend code

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,11 @@
+// const sqlite3 = require('sqlite3').verbose();
+// const db = new sqlite3.Database('./database/tasks.db');
+
+const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./database/tasks.db');
+
+const dbPath = path.join(__dirname, '../database/tasks.db');
+const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS tasks (
